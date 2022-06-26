8f4cf05eea08687e350640a8dd47409f94fd05e1
Vipin Dagar <vipindagar7@users.noreply.github.com>
2022-06-26 20:24:15 +0530

    webtechproject

---
+++ script.js
@@ -0,0 +1,213 @@
+// open signup popup
+function signup(){
+    document.getElementById('home').style.opacity="0.5"
+    document.getElementById('navbar').style.opacity="0.5"
+    document.getElementById("signup").style.display="block"
+   
+ 
+}
+function closesignup(){
+    document.getElementById('home').style.opacity="1"
+    document.getElementById('navbar').style.opacity="1"
+    document.getElementById("signup").style.display="none"
+}
+// open login popup
+function login(){
+    document.getElementById('home').style.opacity="0.5"
+    document.getElementById('navbar').style.opacity="0.5"
+    document.getElementById("login").style.display="block"
+ 
+}
+function closelogin(){
+    document.getElementById('home').style.opacity="1"
+    document.getElementById('navbar').style.opacity="1"
+    document.getElementById("login").style.display="none"
+}
+
+
+// book now popup
+function booknow(){
+    document.getElementById('booknowform').style.display='block'
+    document.getElementById('booknowform').style.zIndex='1'
+}
+function closebooknow(){
+    document.getElementById('booknowform').style.display='none'
+
+}
+
+
+
+
+
+
+// function to open delhi list
+function delhi(){
+    document.getElementById('home').style.display='none'
+    document.getElementById('delhiList').style.display='block'
+    document.getElementById('mumbaiList').style.display='none'
+    document.getElementById('bangloreList').style.display='none'
+    document.getElementById('hyderabadList').style.display='none'
+    document.getElementById("navbar").style.marginTop="-100px"
+}
+// function to open mumbai list
+function mumbai(){
+    document.getElementById('home').style.display='none'
+    document.getElementById('delhiList').style.display='none'
+    document.getElementById('mumbaiList').style.display='block'
+    document.getElementById('bangloreList').style.display='none'
+    document.getElementById('hyderabadList').style.display='none'
+    document.getElementById("navbar").style.marginTop="-100px"
+}
+// function to open banglore list
+function banglore(){
+    document.getElementById('home').style.display='none'
+    document.getElementById('delhiList').style.display='none'
+    document.getElementById('mumbaiList').style.display='none'
+    document.getElementById('bangloreList').style.display='block'
+    document.getElementById('hyderabadList').style.display='none'
+    document.getElementById("navbar").style.marginTop="-100px"
+}
+// function to open hyderabad list
+function hyderabad(){
+    document.getElementById('home').style.display='none'
+    document.getElementById('delhiList').style.display='none'
+    document.getElementById('mumbaiList').style.display='none'
+    document.getElementById('bangloreList').style.display='none'
+    document.getElementById('hyderabadList').style.display='block'
+    document.getElementById("navbar").style.marginTop="-100px"
+}
+// to open participate details
+function openme(){
+    document.getElementById("me").style.display="block"
+}
+function closeme(){
+    document.getElementById("me").style.display="none"
+}
+
+// to open opensaraswatidelhipg details
+function opensaraswatidelhipg(){
+document.getElementById('opensaraswatidelhipg').style.display="block"
+
+}
+function closesaraswatidelhipg(){
+document.getElementById('opensaraswatidelhipg').style.display="none"
+
+}
+// to open satyaniketendelhipg details
+function opensatyaniketendelhipg(){
+document.getElementById('opensatyaniketendelhipg').style.display="block"
+
+}
+function closesatyaniketendelhipg(){
+document.getElementById('opensatyaniketendelhipg').style.display="none"
+
+}
+// to openajantahousedelhipg details
+function openajantahousedelhipg(){
+document.getElementById('openajantahousedelhipg').style.display="block"
+
+}
+function closeajantahousedelhipg(){
+document.getElementById('openajantahousedelhipg').style.display="none"
+
+}
+// to open openxyzmumbaipg details
+function openxyzmumbaipg(){
+document.getElementById('openxyzmumbaipg').style.display="block"
+
+}
+function closexyzmumbaipg(){
+document.getElementById('openxyzmumbaipg').style.display="none"
+
+}
+// to open abcmumbaipg details
+function openabcmumbaipg(){
+document.getElementById('openabcmumbaipg').style.display="block"
+
+}
+function closeabcmumbaipg(){
+document.getElementById('openabcmumbaipg').style.display="none"
+
+}
+// to open123banglorepg details
+function open123banglorepg(){
+document.getElementById('open123banglorepg').style.display="block"
+
+}
+function close123banglorepg(){
+document.getElementById('open123banglorepg').style.display="none"
+
+}
+// to open789banglorepg details
+function open789banglorepg(){
+    document.getElementById('open789banglorepg').style.display="block"
+
+}
+function close789banglorepg(){
+document.getElementById('open789banglorepg').style.display="none"
+
+}
+// to openpqrbanglorepg details
+function openpqrbanglorepg(){
+document.getElementById('openpqrbanglorepg').style.display="block"
+
+}
+function closepqrbanglorepg(){
+document.getElementById('openpqrbanglorepg').style.display="none"
+
+}
+// to open567banglorepg details
+function open567banglorepg(){
+    document.getElementById('open567banglorepg').style.display="block"
+
+}
+function close567banglorepg(){
+    document.getElementById('open567banglorepg').style.display="none"
+
+}
+// to opena2zhyderabadpg details
+function opena2zhyderabadpg(){
+    document.getElementById('opena2zhyderabadpg').style.display="block"
+
+}
+function closea2zhyderabadpg(){
+    document.getElementById('opena2zhyderabadpg').style.display="none"
+
+}
+// to ghihyderabadpg details
+function openghihyderabadpg(){
+    document.getElementById('openghihyderabadpg').style.display="block"
+
+}
+function closeghihyderabadpg(){
+    document.getElementById('openghihyderabadpg').style.display="none"
+
+}
+// to mrfhyderabadpg details
+function openmrfhyderabadpg(){
+    document.getElementById('openmrfhyderabadpg').style.display="block"
+
+}
+function closemrfhyderabadpg(){
+    document.getElementById('openmrfhyderabadpg').style.display="none"
+
+}
+// to rtohyderabadpg details
+function openrtohyderabadpg(){
+    document.getElementById('openrtohyderabadpg').style.display="block"
+
+}
+function closertohyderabadpg(){
+    document.getElementById('openrtohyderabadpg').style.display="none"
+
+}
+// to denhyderabadpg details
+function opendenhyderabadpg(){
+    document.getElementById('opendenhyderabadpg').style.display="block"
+
+}
+function closedenhyderabadpg(){
+    document.getElementById('opendenhyderabadpg').style.display="none"
+
+}
+
